d5f3fb3837fb2171f55b07427749eacdcb7e8f75
Katmore2727 <katmoreyash2121@gmail.com>
2024-06-15 16:39:32 +0530

    Updated Code

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const db = require("./config/db");
+const useroutes = require('./routes/userRoutes');
+const cors = require('cors');
+
+const corsOptions={
+    origin :'http://localhost:3000',
+    optionsSuccessStatus :200
+};
+app.use(cors(corsOptions)) ;
 
 const PORT = process.env.PORT || 5000;
-console.log(process.env.MONGO_URL)
+
+app.use('/api/user', useroutes)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
