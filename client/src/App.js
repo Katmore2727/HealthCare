69081ac5c1eb340cdd4d8e07d01bec468625c63f
Katmore2727 <katmoreyash2121@gmail.com>
2024-07-17 15:18:55 +0530

    Updated Code

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login'; 
 import Register from './pages/Register';
+import Patient from './pages/Patient';
+import Doctor from './pages/Doctor';
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route exact path='/login' element={<Login />} />
       <Route exact path='/register' element={<Register />} />
+      <Route exact path='/patient' element={<Patient />} />
+      <Route exact path='/doctor' element={<Doctor/>} />
     </Routes>
     </BrowserRouter>
 
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const db = require("./config/db");
-const useroutes = require('./routes/userRoutes');
+const router = require('./routes/userRoutes');
 const cors = require('cors');
+const bodyParser = require('body-parser'); 
 
 const corsOptions={
     origin :'http://localhost:3000',
@@ -12,8 +13,8 @@ const corsOptions={
 app.use(cors(corsOptions)) ;
 
 const PORT = process.env.PORT || 5000;
-
-app.use('/api/user', useroutes)
+app.use(express.json());
+app.use('/api/user', router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
